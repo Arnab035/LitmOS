fc73f48a4b2de45c5e98c04de0374d4e4023ccb5
Arnab035 <arnabjyotikalita35@gmail.com>
2017-01-13 23:20:15 -0500

    added useful helper methods for pic

--- pic.cpp
+++ pic.cpp
@@ -2,6 +2,9 @@
 							Implementation of methods in PIC microcontroller
 *******************************************************************************************/
 
+#include "pic.h"
+
+
 //pic1 register port addresses abstracted
 
 #define I86_PIC1_REG_COMMAND 0x20
@@ -56,6 +59,76 @@
 #define I86_PIC_ICW4_BUF_MODENO 0
 #define I86_PIC_ICW4_SFNM_NESTEDMODE 0x10
 #define I86_PIC_ICW4_SFNM_NOTNESTED 0
+
+// useful functions defined 
+
+/* send command to the appropriate pic register */
+inline void i86_pic_send_command(uint8_t cmd, uint8_t picNum) {
+	if(picNum > 1) return;
+	
+	uint8_t reg = (picNum == 1) ? I86_PIC2_REG_COMMAND : I86_PIC1_REG_COMMAND;
+	outportb(reg, cmd);             // defined in hal.h
+
+}
+
+/* write data into the appropriate pic data register --> this is actually the interrupt mask register */
+
+inline void i86_pic_send_data(uint8_t data, uint8_t picNum) {
+	if(picNum > 1) return ;
+	
+	uint8_t reg = (picNum==1) ? I86_PIC2_REG_DATA : I86_PIC1_REG_DATA ;
+	outportb(reg, data);
+
+}
+
+/* read data from the appropriate pic data register*/
+
+inline uint8_t i86_pic_read_data(uint8_t picNum){
+	if(picNum > 1) return;
+	
+	uint8_t reg = (picNum==1) ? I86_PIC2_REG_DATA : I86_PIC1_REG_DATA ;
+	return inportb(reg);
+}
+
+
+/* initialize pic using the initialization control words -- discussed */
+
+void i86_pic_initialize(uint8_t base0, uint8_t base1){
+	
+	// interestingly enough only the first icw is a command byte --> the rest are data bytes
+	uint8_t icw = 0;
+	
+	icw = (icw & ~I86_PIC_ICW1_MASK_INIT) | I86_PIC_ICW1_INIT_YES ;
+	
+	icw = (icw & ~I86_PIC_ICW1_MASK_IC4) | I86_PIC_ICW1_IC4_EXPECT ;
+	i86_pic_send_command(icw, 0);
+	
+	i86_pic_send_command(icw, 1);
+	
+	// send the icw 2. this byte contains the base interrupt number for the pic
+	
+	i86_pic_send_data(base0, 0);
+	i86_pic_send_data(base1, 0);
+	
+	// send the icw3. icw3 for master pic is the irq that connects to the secondary pic in binary format.
+	//icw3 for secondary pic is the irq that connects to the master pic in decimal format
+	
+	i86_pic_send_data(0x04, 0);
+	i86_pic_send_data(0x02, 0);
+	
+	// send the icw4 --> make the mode as x86.
+	
+	icw = (icw & ~I86_PIC_ICW4_MASK_UPM) | I86_PIC_ICW4_UPM_86MODE ;
+	
+	i86_pic_send_data(icw, 0);
+	i86_pic_send_data(icw, 1);
+	
+}
+
+
+
+
+
 
 
 
