01f2e7cb9e2cd09b79fabd716fe940b5ce58156f
Arnab035 <arnabjyotikalita35@gmail.com>
2017-01-19 23:06:56 -0500

    added the remaining functions for virtual file system

--- fsys.cpp
+++ fsys.cpp
@@ -38,3 +38,47 @@ FILE volOpenFile(const char* fname){
 	file.flags = FS_INVALID;
 	return file;
 }
+
+void volRegisterFileSystem(PFILESYSTEM fsys, unsigned int deviceId){
+	
+	if(deviceId < MAX_DEVICE){
+		if(fsys){
+			_file_system_list[deviceId] = fsys ; // register file system for the particular device
+		}
+	}
+}
+
+// define the other functions
+
+void volDeRegisterFileSystem(PFILESYSTEM fsys){
+	if(!fsys) return;
+	// iterate through all the devices in the device list
+	for(int i = 0; i< MAX_DEVICE; i++){
+		if(_file_system_list[i] == fsys)
+			_file_system_list[i] = 0;
+	}
+}
+
+void volDeRegisterFileSystemByID(unsigned int deviceId){
+	if(deviceId >= MAX_DEVICE) return;
+	
+	_file_system_list[deviceId] = 0;
+}
+
+void volCloseFile(PFILE file){
+	// get me the device where the file is in
+	if(!file) return;
+	if(_file_system_list[file->device - 'a'])
+		(_file_system_list[file->device - 'a'])->close(file); 
+}
+
+
+void volReadFile(PFILE file, unsigned char* buffer, unsigned int length){
+	if(!file) return;
+	// identify which file system the file is in --> then call the method defined for that file system
+	if(_file_system_list[file->device - 'a'])
+		(_file_system_list[file->device - 'a'])->read(file, buffer, length);
+	
+}
+
+
